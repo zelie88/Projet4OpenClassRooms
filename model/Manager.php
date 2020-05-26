b89706373a89820b6c859b478c8198ddd202e422
Zelie Alchami <zelie.alchami@gmail.com>
2020-05-26 19:01:44 +0200

    Passage en objet du modèle

--- model/Manager.php
+++ model/Manager.php
@@ -1,23 +1,9 @@
 <?php
-
 class Manager
 {
-    const Db_HOST='mysql:host=localhost;dbname=blog;charset=utf8';
-    const Db_USER='root';
-    const Db_PASS='';
-
-    protected function getConnection()
+    protected function dbConnect()
     {
-        try{
-            $connection=new PDO(self::Db_HOST,self::Db_USER,self::Db_PASS);
-            $connection->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-            return $connection;
-        }
-
-        catch(Exception $errorConnection)
-        {
-            die('Erreur de connexion:'.$errorConnection->getMessage());
-
-        }
+            $db = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'zelie', '');
+            return $db;
     }
 }
--- model/PostManager.php
+++ model/PostManager.php
@@ -1,23 +1,23 @@
 <?php
-require 'model/Manager.php';
+require_once("Manager.php");
 
 class PostManager extends Manager
 {
-    public function getArticles()
+    public function getPosts()
     {
-        $connection = $this->getConnection();
-        $response = $connection->query('SELECT id, title, content, DATE_FORMAT(creationDate, \'%d/%m/%Y à %Hh%imin%ss\') AS creationDate FROM articles ORDER BY creationDate DESC LIMIT 0, 5');
-
-        return $response;
+        $db = $this->dbConnect();
+        $req = $db->query('SELECT id, title, content, DATE_FORMAT(creationDate, \'%d/%m/%Y à %Hh%imin%ss\') AS creationDate FROM articles ORDER BY creationDate DESC LIMIT 0, 5');
+        
+        return $req;
     }
 
-    public function getArticle($articleId)
+    public function getPost($postId)
     {
-        $connection = $this->getConnection();
-        $response = $connection->prepare('SELECT id, title, content, DATE_FORMAT(creationDate, \'%d/%m/%Y à %Hh%imin%ss\') AS creationDate FROM articles WHERE id=?');
-        $response->execute([$articleId]);
-        $article = $response->fetch();
-
-        return $article;
+        $db = $this->dbConnect();
+        $req = $db->prepare('SELECT id, title, content, DATE_FORMAT(creationDate, \'%d/%m/%Y à %Hh%imin%ss\') AS creationDate FROM articles WHERE id = ?');
+        $req->execute(array($postId));
+        $post = $req->fetch();
+        
+        return $post;
     }
 }
