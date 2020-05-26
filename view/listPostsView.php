6d6c3db5be5993b54a6efd28ea864c1301556827
Zelie Alchami <zelie.alchami@gmail.com>
2020-05-26 20:15:36 +0200

    Ajout du formulaire de commentaires

---
+++ view/listPostsView.php
@@ -0,0 +1,29 @@
+<?php $title = 'Billet simple pour l\'Alaska'; ?>
+        
+    <?php ob_start(); ?>
+        <h1>Billet simple pour l'Alaska</h1>
+        <p>Derniers billets du blog :</p>
+ 
+        
+        <?php
+        while ($data = $posts->fetch())
+        {
+        ?>
+            <div class="news">
+                <h3>
+                    <?= htmlspecialchars($data['title']) ?>
+                    <em>le <?= $data['creationDate'] ?></em>
+                </h3>
+                
+                <p>
+                    <?= nl2br(htmlspecialchars($data['content'])) ?>
+                    <br />
+                    <em><a href="index.php?action=post&amp;id=<?= $data['id'] ?>">Commentaires</a></em>
+                </p>
+            </div>
+        <?php
+        }
+        $posts->closeCursor();
+        ?>
+    <?php $content = ob_get_clean(); ?>
+<?php require('template.php'); ?>
---
+++ view/template.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title><?= $title ?></title>
+        <link href="public/css/style.css" rel="stylesheet"/>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    </head>
+        
+    <body>
+        <?= $content ?>
+    </body>
+</html>
