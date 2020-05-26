5bb74938106f061202f0d3e3ead6eca8b071d166
Zelie Alchami <zelie.alchami@gmail.com>
2020-05-26 20:36:18 +0200

    Ajout des namespaces

--- model/Manager.php
+++ model/Manager.php
@@ -1,9 +1,12 @@
 <?php
+
+namespace Projet4OpenClassRooms\Model;
+
 class Manager
 {
     protected function dbConnect()
     {
-            $db = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'zelie', '');
+            $db = new \PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'zelie', '');
             return $db;
     }
 }
