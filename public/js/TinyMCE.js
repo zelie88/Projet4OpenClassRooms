f0ba947e4b84e3cc2632e195d1b080f1ab483961
Zelie Alchami <zelie.alchami@gmail.com>
2020-08-15 16:30:22 +0200

    Fichier User.js pour gestion du mot de passe

--- public/js/TinyMCE.js
+++ public/js/TinyMCE.js
@@ -5,13 +5,3 @@ tinymce.init({
     plugins: 'advlist autolink lists link image charmap print preview hr anchor pagebreak',
     toolbar_mode: 'floating',
 });
-
-function Toggle() { 
-    var temp = document.getElementById("password"); 
-    if (temp.type === "password") { 
-        temp.type = "text"; 
-    } 
-    else { 
-        temp.type = "password"; 
-    } 
-}
