1cf6abbda474fa972350483c98177bbda3acbecf
Zelie Alchami <zelie.alchami@gmail.com>
2020-10-21 15:55:33 +0200

    Présentation de l'auteur

---
+++ templates/author.php
@@ -0,0 +1,11 @@
+<div class="card" style="width: 18rem;">
+
+<h3 class="card-title">L'auteur</h3>
+    <img src="../public/img/JeanForteroche.png" class="card-img-top">
+    
+    <div class="card-body">
+        
+        <p class="card-text"><strong>Jean Forteroche</strong> est un auteur voyageur né le 29 avril 1930 à Paris. Son dernier roman vous emmènera, chapitres après chapitres publiés sur son blog, jusqu'en Alaska. Le billet sera-t-il sans retour?</p>
+    </div>
+</div>
+<br>
